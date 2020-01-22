5f23b917a257acf78f4144b9a34f35ba9e48dbf0
Joo Sung <joohsung@gmail.com>
2020-01-21 21:39:23 -0500

    AAI V.1.5.1

--- APM/phpMyAdmin.sh
+++ APM/phpMyAdmin.sh
@@ -19,6 +19,13 @@
 
 yum install -y phpmyadmin
 
+sed -i 's/Require ip 127.0.0.1/#Require ip 127.0.0.1/' /etc/httpd/conf.d/phpMyAdmin.conf
+sed -i 's/Require ip ::1/#Require ip ::1/' /etc/httpd/conf.d/phpMyAdmin.conf
+sed -i '/Require ip ::1/a\       Require all granted' /etc/httpd/conf.d/phpMyAdmin.conf
+sed -i 's/cookie/http/' /etc/phpMyAdmin/config.inc.php
+
+sh /root/AAI/restart.sh
+
 echo ""
 echo ""
 echo "축하 드립니다. phpMyAdmin 설치 작업이 끝났습니다."
